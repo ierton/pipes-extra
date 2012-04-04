67a26e912d1d814479aedef4c22ef3a3b54701cd
Sergey Mironov <ierton@gmail.com>
2012-04-05 02:14:50 +0400

    Reimplement PutbackPipe without using loopP
    loopP has a special effect: user can't send anything to themself after tryAwait
    returning Nothing. New implementation allows it.

--- pipes-extra/Control/Pipe/PutbackPipe.hs
+++ pipes-extra/Control/Pipe/PutbackPipe.hs
@@ -1,11 +1,14 @@
 module Control.Pipe.PutbackPipe where
 
+import Control.Monad.State
+import Control.Monad.Trans
+
 import Control.Pipe
 import Control.Pipe.Combinators
 import Control.Pipe.Monoidal
 
 newtype PutbackPipe a b m r = PutbackPipe {
-    unPutback :: Pipe a (Either b a) m r
+    unPutback :: StateT [a] (Pipe a b m) r
     }
 
 instance (Monad m) => Monad (PutbackPipe a b m) where
@@ -13,20 +16,24 @@ instance (Monad m) => Monad (PutbackPipe a b m) where
     (PutbackPipe p) >>= f = PutbackPipe (p >>= unPutback . f)
 
 nonputback :: (Monad m) => Pipe a b m r -> PutbackPipe a b m r
-nonputback p = PutbackPipe (p >+> pipe Left)
+nonputback p = PutbackPipe (lift p)
 
 putback :: (Monad m) => a -> PutbackPipe a b m ()
-putback = PutbackPipe . yield . Right
+putback a = PutbackPipe (modify (a:))
 
 yieldPB :: (Monad m) => b -> PutbackPipe a b m ()
-yieldPB = PutbackPipe . yield . Left
+yieldPB = PutbackPipe . lift . yield
 
 awaitPB  :: (Monad m) => PutbackPipe a b m a
-awaitPB  = PutbackPipe await
+awaitPB = PutbackPipe (get >>= decide) where
+    decide [] = lift await
+    decide (a:as) = put as >> return a
 
 tryAwaitPB  :: (Monad m) => PutbackPipe a b m (Maybe a)
-tryAwaitPB  = PutbackPipe tryAwait
+tryAwaitPB = PutbackPipe (get >>= decide) where
+    decide [] = lift tryAwait
+    decide (a:as) = put as >> return (Just a)
 
 runPutback :: (Monad m) => PutbackPipe a b m r -> Pipe a b m r
-runPutback pb = loopP (joinP >+> (unPutback pb))
-        
+runPutback pb = evalStateT (unPutback pb) []
+
