355819c05e5cadf41a7e9e8844ec53c814e27997
Sergey Mironov <ierton@gmail.com>
2012-04-09 14:16:42 +0400

    PutbackPipe: unify 0.0.1 and 0.1 interfaces

--- pipes-extra/Control/Pipe/PutbackPipe.hs
+++ pipes-extra/Control/Pipe/PutbackPipe.hs
@@ -4,17 +4,13 @@ import Control.Monad.State
 import Control.Monad.Trans
 import Control.Applicative
 
-import Control.Pipe
-import Control.Pipe.Combinators
-import Control.Pipe.Monoidal
+import Control.Pipe as P
+import Control.Pipe.Combinators as P
+import Control.Pipe.Monoidal as P
 
 newtype PutbackPipe a b m r = PutbackPipe {
     unPutback :: StateT [a] (Pipe a b m) r
     }
-
--- | FIXME: Move this to pipes-core
-instance (MonadIO m) => MonadIO (Pipe a b m)  where
-    liftIO = lift . liftIO
 
 instance (Monad m) => Monad (PutbackPipe a b m) where
     return = PutbackPipe . return
@@ -23,26 +19,29 @@ instance (Monad m) => Monad (PutbackPipe a b m) where
 instance (Monad m, Functor m) => Functor (PutbackPipe a b m) where
     fmap f (PutbackPipe p) = PutbackPipe (fmap f p)
 
+instance (MonadIO m) => MonadIO (Pipe a b m) where
+    liftIO = lift . liftIO
+
 instance (MonadIO m) => MonadIO (PutbackPipe a b m) where
     liftIO a = PutbackPipe (liftIO a)
 
-nonputback :: (Monad m) => Pipe a b m r -> PutbackPipe a b m r
-nonputback p = PutbackPipe (lift p)
+fromPipe :: (Monad m) => Pipe a b m r -> PutbackPipe a b m r
+fromPipe p = PutbackPipe (lift p)
 
 putback :: (Monad m) => a -> PutbackPipe a b m ()
 putback a = PutbackPipe (modify (a:))
 
-yieldPB :: (Monad m) => b -> PutbackPipe a b m ()
-yieldPB = PutbackPipe . lift . yield
+yield :: (Monad m) => b -> PutbackPipe a b m ()
+yield = PutbackPipe . lift . P.yield
 
-awaitPB  :: (Monad m) => PutbackPipe a b m a
-awaitPB = PutbackPipe (get >>= decide) where
-    decide [] = lift await
+await  :: (Monad m) => PutbackPipe a b m a
+await = PutbackPipe (get >>= decide) where
+    decide [] = lift P.await
     decide (a:as) = put as >> return a
 
-tryAwaitPB  :: (Monad m) => PutbackPipe a b m (Maybe a)
-tryAwaitPB = PutbackPipe (get >>= decide) where
-    decide [] = lift tryAwait
+tryAwait :: (Monad m) => PutbackPipe a b m (Maybe a)
+tryAwait = PutbackPipe (get >>= decide) where
+    decide [] = lift P.tryAwait
     decide (a:as) = put as >> return (Just a)
 
 runPutback :: (Monad m) => PutbackPipe a b m r -> Pipe a b m r
